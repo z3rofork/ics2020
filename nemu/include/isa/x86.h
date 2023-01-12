44251113799727e494f47fe87eae3b5ae8b0744e
tracer-ics2020 <tracer@njuics.org>
2023-01-11 21:16:51 -0500

    >  compile 114514  Static hostname: normal        Icon name: computer-vm          Chassis: vm       Machine ID: 8283e071490343188007b10cb544d01b          Boot ID: e4195ea493c34d77b4b482ae84fb3d91   Virtualization: vmware Operating System: Linux Mint 21.1           Kernel: Linux 5.15.0-56-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  21:16:52 up  1:39,  1 user,  load average: 0.46, 0.46, 0.45

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -21,8 +21,9 @@ typedef struct {
   union {
     uint32_t _32;
     uint16_t _16;
-    uint8_t _8[2];
-    struct 
+    uint8_t _8[2];   
+  } gpr[8];
+ struct 
     {
     /* registers */
     uint32_t eax;
@@ -34,15 +35,13 @@ typedef struct {
     uint32_t esi;
     uint32_t edi;
     }; 
-  } gpr[8];
-
   /* Do NOT change the order of the GPRs' definitions. */
 
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
    
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  //rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
   vaddr_t pc;
 } x86_CPU_state;
