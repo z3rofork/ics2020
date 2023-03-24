bbd2f8c2da0e0acf95a3dccb898de5b84c5c3bbb
tracer-ics2020 <tracer@njuics.org>
2023-03-24 02:09:51 -0400

    >  compile 114514  Static hostname: normal        Icon name: computer-vm          Chassis: vm       Machine ID: 8283e071490343188007b10cb544d01b          Boot ID: 99448aff43814b2e91b6e824d603d454   Virtualization: vmware Operating System: Linux Mint 21.1           Kernel: Linux 5.15.0-60-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  02:09:51 up 1 day,  5:45,  3 users,  load average: 0.10, 0.04, 0.01

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -26,6 +26,7 @@ static inline def_EHelper(call) {
   // the target address is calculated at the decode stage
   //TODO();
   rtl_push(s,ddest);
+  rtl_j(s,s->jmp_pc);
   print_asm("call %x", s->jmp_pc);
 }
 
